5e85ede3832d35f1f10ecdb52824f9e9fd746d56
Luiz Henrique Goulart <lhgoulart92@yahoo.com.br>
2019-08-04 19:22:20 -0300

    Ajustes no endpoint /categoria/{id}

---
+++ src/main/java/com/lhgoulart/cursomc/resources/excpetion/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.lhgoulart.cursomc.resources.excpetion;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.lhgoulart.cursomc.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> ObjectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+}
